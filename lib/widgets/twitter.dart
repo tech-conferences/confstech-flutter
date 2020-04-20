4e29a523611ddc34c0bfef8bac445c0b029ec0a6
leonardo2204 <ferrari2204_5@hotmail.com>
2020-04-20 17:51:29 -0300

    Add twitter link

--- lib/widgets/twitter.dart
+++ lib/widgets/twitter.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Twitter extends StatelessWidget {
 
@@ -10,18 +12,28 @@ class Twitter extends StatelessWidget {
   Widget build(BuildContext context) {
     if (twitter == null) { return Container(); }
 
+    final String twitterUrl = "https://www.twitter.com/$twitter";
+
     return Row(
       children: <Widget>[
-        Text('・',
-            style: TextStyle(
-              fontWeight: FontWeight.w300,
-            )
+        InkWell(
+          child: Text('・ $twitter',
+              style: TextStyle(
+                fontWeight: FontWeight.w300,
+              )
+          ),
+          onTap: () async {
+            if (await canLaunch(twitterUrl)) {
+              await launch(twitterUrl);
+            } else {
+              Scaffold.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Something bad happened :('),
+                  )
+              );
+            }
+          },
         ),
-        Text(this.twitter,
-            style: TextStyle(
-              fontWeight: FontWeight.w300,
-            )
-        )
       ],
     );
   }
