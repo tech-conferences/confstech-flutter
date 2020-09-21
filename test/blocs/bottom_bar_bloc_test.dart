f41a2a6dd4566e63e4a7f1c73cf7755b56e2cee5
Ryan Edge <ryan.c.edge@outlook.com>
2020-09-21 02:34:53 -0400

    feat: added test for bottom bar bloc

---
+++ test/blocs/bottom_bar_bloc_test.dart
@@ -0,0 +1,35 @@
+// This is a basic Flutter widget test.
+//
+// To perform an interaction with a widget in your test, use the WidgetTester
+// utility that Flutter provides. For example, you can send tap and scroll
+// gestures. You can also use WidgetTester to find child widgets in the widget
+// tree, read text, and verify that the values of widget properties are correct.
+
+import 'package:confs_tech/blocs/bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bloc_test/bloc_test.dart';
+
+void main() {
+  group('BottomBarBloc', () {
+    BottomBarBloc bottomBarBloc;
+
+    setUp(() {
+      bottomBarBloc = BottomBarBloc();
+    });
+
+    tearDown(() {
+      bottomBarBloc?.close();
+    });
+
+    test('initial state.selectedIndex is 0', () {
+      expect(bottomBarBloc.state.selectedIndex, 0);
+    });
+
+    blocTest(
+      'emits [1] when CounterEvent.increment is added',
+      build: () => bottomBarBloc,
+      act: (BottomBarBloc bloc) => bloc.add(BottomBarSelected(1)),
+      expect: [BottomBarSelectedSuccess(selectedIndex: 1)],
+    );
+  });
+}
